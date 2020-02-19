6ea3c7c0ca5997ad804a764942091f8b5eec6aa4
badat <you@example.com>
2020-02-19 09:09:17 +0700

    KieuDulIEU

---
+++ bai3.js
@@ -0,0 +1,11 @@
+let a =prompt( "nhap bien a");
+alert(a);
+let b = prompt("nhap bien b ");
+alert(b)
+if(a % b==0)
+{
+    alert('a chia het cho b');
+}
+else{
+    alert('a khong chia het cho b');
+}
